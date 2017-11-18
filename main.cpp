737b9cc8ca44caddd75b290148f92cdc19b92806
Jonathan Boccara <boccaraj@gmail.com>
2017-11-18 21:10:55 +0100

    Use Catch 2 for unit tests

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include "defaulted.h"
-namespace
-{
-namespace test
-{
-        
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
 int f1(int , fluent::Defaulted<int, 42> y, int )
 {
     return y.get();
 }
 
-bool testDefaultValue()
+TEST_CASE("Default value", "[Defaulted]")
 {
-    return f1(0, fluent::defaultValue, 1) == 42;
+    REQUIRE(f1(0, fluent::defaultValue, 1) == 42);
 }
 
-bool testValueProvided()
+TEST_CASE("Value provided", "[Defaulted]")
 {
-    return f1(0, 43, 1) == 43;
+    REQUIRE(f1(0, 43, 1) == 43);
 }
 
 std::string f2(int , fluent::Defaulted<std::string> y, int )
@@ -25,14 +24,14 @@ std::string f2(int , fluent::Defaulted<std::string> y, int )
     return y.get();
 }
 
-bool testDefaultConstructor()
+TEST_CASE("Default constructor", "[Defaulted]")
 {
-    return f2(0, fluent::defaultValue, 1) == std::string();
+    REQUIRE(f2(0, fluent::defaultValue, 1) == std::string());
 }
 
-bool testDefaultConstructorValueProvided()
+TEST_CASE("Default constructor value provided", "[Defaulted]")
 {
-    return f2(0, std::string("hello"), 1) == "hello";
+    REQUIRE(f2(0, std::string("hello"), 1) == "hello");
 }
 
 struct GetDefaultAmount{ static double get(){ return 45.6; } };
@@ -42,14 +41,14 @@ double f3(int x, fluent::DefaultedF<double, GetDefaultAmount> y, int z)
     return y.get();
 }
 
-bool testDefaultedF()
+TEST_CASE("DefaultedF Default value", "[DefaultedF]")
 {
-    return f3(0, fluent::defaultValue, 1) == 45.6;
+    REQUIRE(f3(0, fluent::defaultValue, 1) == 45.6);
 }
 
-bool testDefaultedFValueProvided()
+TEST_CASE("DefaultedF Value provided", "[DefaultedF]")
 {
-    return f3(0, 13.4, 1) == 13.4;
+    REQUIRE(f3(0, 13.4, 1) == 13.4);
 }
     
 class CopyLogger
@@ -63,52 +62,18 @@ private:
     
 void f4(fluent::Defaulted<CopyLogger>){}
 
-bool testDefaultedMakesACopy()
+TEST_CASE("Defaulted makes a copy", "[Defaulted]")
 {
     int copyCount = 0;
     f4(CopyLogger(copyCount));
-    return copyCount == 1;
+    REQUIRE(copyCount == 1);
 }
     
 void f5(fluent::Defaulted<CopyLogger const&>){}
     
-bool testDefaultedConstRefMakesNoCopy()
+TEST_CASE("Defaulted const ref makes no copy", "[Defaulted]")
 {
     int copyCount = 0;
     f5(CopyLogger(copyCount));
-    return copyCount == 0;
-}
-
-    
-template <typename TestFunction>
-bool launchTest(std::string const& testName, TestFunction testFunction)
-{
-    const bool success = testFunction();
-    if (!success)
-        std::cout << "Test - " << testName << ": FAILED\n";
-    return success;
-}
-
-void launchTests()
-{
-    bool success = true;
-    success &= launchTest("Default value", testDefaultValue);
-    success &= launchTest("Value provided", testValueProvided);
-    success &= launchTest("Default constructor", testDefaultConstructor);
-    success &= launchTest("Default constructor value provided", testDefaultConstructorValueProvided);
-    success &= launchTest("DefaultedF", testDefaultedF);
-    success &= launchTest("DefaultedF value provided", testDefaultedFValueProvided);
-    success &= launchTest("Defaulted makes a copy", testDefaultedMakesACopy);
-    success &= launchTest("Defaulted const& makes no copy", testDefaultedConstRefMakesNoCopy);
-
-    if (success)
-        std::cout << "All tests PASSED\n";
-}
-}
-}
-
-
-int main()
-{
-    test::launchTests();
+    REQUIRE(copyCount == 0);
 }
